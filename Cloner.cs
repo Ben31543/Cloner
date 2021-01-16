27a1bca60c37df92df47f9bfa0e5dc9d996c4864
Ben Atanesyan <benatanesyan2@gmail.com>
2021-01-16 20:24:57 +0400

    Add project files.

---
+++ Cloner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObjectCloner
+{
+    static class Cloner
+    {
+        public static void GetDeepClone<T>(T cloneFrom, T cloneTo)
+        {
+            Type cloneableType = typeof(T);
+            MemberInfo[] members = cloneableType.GetProperties();
+
+            foreach (PropertyInfo propertyInfo in members)
+            {
+                PropertyInfo property = cloneableType.GetProperty(propertyInfo.Name);
+                property.SetValue(cloneTo, property.GetValue(cloneFrom, null));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ObjectCloner
+{
+    public class User
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public Account Account { get; set; }
+    }
+
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string Nickname { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User
+            {
+                Age = 17,
+                Name = "Ben",
+                Account = new Account
+                {
+                    Id = 1,
+                    Nickname = "benatanesyan"
+                }
+            };
+
+            var anotherUser = new User();
+
+            Cloner.GetDeepClone<User>(user, anotherUser);
+
+            user.Age = 40;
+            user.Account.Id = 16;
+
+            Console.WriteLine($"user: {user.Age}");
+            Console.WriteLine($"newUser: {anotherUser.Age}");
+
+            Console.WriteLine($"user: {user.Account.Id}");
+            Console.WriteLine($"newUser: {anotherUser.Account.Id}");
+        }
+    }
+}
